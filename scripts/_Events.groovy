f25e2ca3541131d7bc02bc285dd9e645a9c6f89f
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-09-12 16:33:35 +0100

    Moved code around so that it matches the default implementation of the assets pipeline. i.e. Created a LessProcessor and set that against the AssetFile
    This is useful as other processors can be inserted to act before less
    compilation.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,9 +1,5 @@
 // includeTargets << new File(assetPipelinePluginDir, "scripts/_AssetCompile.groovy")
 
-eventAssetPrecompileStart = { assetConfig ->
-	def lessAssetFile = classLoader.loadClass('asset.pipeline.less.LessAssetFile')
-	if(config?.grails?.assets?.less?.compiler == 'less4j') {
-		lessAssetFile.compilerMode = 'less4j'	
-	}
+eventAssetPrecompileStart = { assetConfig ->  
 	assetConfig.specs << 'asset.pipeline.less.LessAssetFile'
 }
--- src/groovy/asset/pipeline/less/LessAssetFile.groovy
+++ src/groovy/asset/pipeline/less/LessAssetFile.groovy
@@ -1,56 +1,48 @@
 package asset.pipeline.less
 
+import groovy.util.logging.Log4j;
+
+import org.mozilla.javascript.NativeArray;
+
 import asset.pipeline.AbstractAssetFile
 import asset.pipeline.AssetHelper
 import asset.pipeline.CacheManager
+import asset.pipeline.less.LessProcessor
+import asset.pipeline.less.compilers.*
 import asset.pipeline.processors.CssProcessor
-
+@Log4j
 class LessAssetFile extends AbstractAssetFile {
-    static final String contentType = 'text/css'
-    static extensions = ['less', 'css.less']
-    static final String compiledExtension = 'css'
-    static processors = [CssProcessor]
-    static compilerMode = 'standard'
+  public static final java.lang.ThreadLocal threadLocal = new ThreadLocal();
+  static final String contentType = 'text/css'
+  static extensions = ['less', 'css.less']
+  static final String compiledExtension = 'css'
+  static processors = [LessProcessor, CssProcessor]
 
-    String processedStream(precompiler) {
-        def fileText
-        def skipCache = precompiler ?: (!processors || processors.size() == 0)
+  @Override
+  public String directiveForLine (String line) {
+    return null;
+  }
 
-        if (baseFile?.encoding || encoding) {
-            fileText = file?.getText(baseFile?.encoding ? baseFile.encoding : encoding)
-        } else {
-            fileText = file?.text
+  static void print(text) {
+    log.debug text
+  }
+
+  static String resolveUri(String path, NativeArray paths) {
+    def assetFile = threadLocal.get();
+    log.debug "resolveUri: path=${path}"
+    for (Object index : paths.getIds()) {
+      def it = paths.get(index, null)
+      def file = new File(it, path)
+      log.trace "test exists: ${file}"
+      if (file.exists()) {
+        log.trace "found file: ${file}"
+        if (assetFile) {
+          CacheManager.addCacheDependency(assetFile.file.canonicalPath, file)
         }
-
-        def md5 = AssetHelper.getByteDigest(fileText.bytes)
-        if (!skipCache) {
-            def cache = CacheManager.findCache(file.canonicalPath, md5)
-            if (cache) {
-                return cache
-            }
-        }
-
-        def lessProcessor
-        if (compilerMode == 'less4j') {
-            lessProcessor = new Less4jProcessor(precompiler)
-        } else {
-            lessProcessor = new LessProcessor(precompiler)
-        }
-        fileText = lessProcessor.process(fileText, this)
-
-        for (processor in processors) {
-            def processInstance = processor.newInstance(precompiler)
-            fileText = processInstance.process(fileText, this)
-        }
-
-        if (!skipCache) {
-            CacheManager.createCache(file.canonicalPath, md5, fileText)
-        }
-
-        return fileText
+        return file.toURI().toString()
+      }
     }
 
-    String directiveForLine(String line) {
-        line.find(/\*=(.*)/) { fullMatch, directive -> return directive }
-    }
+    return null
+  }
 }
