372887663f472b90f824b3b95a538be5369ef7ca
David Estes <davydotcom@gmail.com>
2015-04-06 08:13:08 -0400

    updating less version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,11 +13,11 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'com.bertramlabs.plugins:less-asset-pipeline:2.0.8'
+		compile 'com.bertramlabs.plugins:less-asset-pipeline:2.1.0'
 	}
 
 	plugins {
-		runtime ":asset-pipeline:2.0.8"
+		runtime ":asset-pipeline:2.1.5"
 
 		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
