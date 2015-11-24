f73cdc67e4f967928fb69f57e2224dbae6b9948d
David Estes <davydotcom@gmail.com>
2015-11-24 11:10:12 -0500

    version bump

--- LessAssetPipelineGrailsPlugin.groovy
+++ LessAssetPipelineGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class LessAssetPipelineGrailsPlugin {
-    def version = "2.6.2"
+    def version = "2.6.7"
     def grailsVersion = "2.2 > *"
     def title = "LESS Asset-Pipeline Plugin"
     def author = "David Estes"
